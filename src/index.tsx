b26ca4c0a69bb93a33a0551bd0f5a4834ec02925
stelleal <stellalleal@gmail.com>
2022-03-24 15:32:50 -0300

    projeto finalizado

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs';
+
 import { App } from './App';
+
+createServer({
+	models: {
+		transaction: Model,
+	},
+
+	seeds(server) {
+		server.db.loadData({
+			transactions: [
+				{
+					id: 1,
+					title: 'Freelance de website',
+					type: 'deposit',
+					category: 'Dev',
+					value: 6000,
+					createdAt: new Date('2022-02-12 09:00:00'),
+				},
+				{
+					id: 2,
+					title: 'Aluguel',
+					type: 'withdraw',
+					category: 'Casa',
+					value: 1100,
+					createdAt: new Date('2022-02-14 10:00:00'),
+				}
+
+			],
+		})
+	},
+
+	routes() {
+		this.namespace = 'api';
+
+		this.get('/transactions', () => {
+			return this.schema.all('transaction')
+		})
+
+		this.post('/transactions', (schema, request) => {
+			const data = JSON.parse(request.requestBody);
+
+			return schema.create('transaction', data)
+		})
+	}
+})
 
 ReactDOM.render(
 	<React.StrictMode>
